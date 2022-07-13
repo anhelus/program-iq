f890b9e97dc876c3823a296cb13c3f626305d54b
Angelo Cardellicchio <angelo.cardellicchio@sstlab.it>
2022-07-13 16:36:27 +0200

    (update) cumulative update

---
+++ _data/blog/draft-cosa-e-rete-neurale.md
@@ -0,0 +1,3 @@
+# Cosa è una rete neurale?
+
+Ricordo ancora come, quando ero tra i banchi universitari, si diceva che uno dei problemi più complessi da 
--- src/pages/teaching.tsx
+++ src/pages/teaching.tsx
@@ -10,9 +10,7 @@ const courses = [
 		url: "https://informatica.angelocardellicchio.it",
 		imgUrl: "/assets/informatica.jpg",
     imgCredits: "https://unsplash.com/photos/cYyqhdbJ9TI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
-		title: "Corso di Informatica",
-		location: "Dipartimento di Matematica",
-    datetime: "A.A. 2020/2021"
+		title: "Computer Science (Italian)",
 	}
 ]
 
@@ -33,11 +31,11 @@ const About = () => {
   return (
     <Layout>
       <Helmet>
-        <title>Didattica — {data.site.siteMetadata.title}</title>
+        <title>Teaching — {data.site.siteMetadata.title}</title>
       </Helmet>
       <div style={{textAlign: "center", padding:"5vh 0", lineHeight: "1.5", fontSize: 18}}>
         <h1>
-          Didattica
+          Teaching
         </h1>
         <div className="grids">
           {
@@ -47,8 +45,8 @@ const About = () => {
                 imgUrl={course.imgUrl}
                 imgCredits={course.imgCredits}
                 title={course.title}
-                location={course.location}
-                datetime={course.datetime}
+                // location={course.location}
+                // datetime={course.datetime}
               />
             })
           }
